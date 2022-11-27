5123e61a6447349efa6ec4d447c9d506ae18de8e
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-27 18:32:44 +0000

    created a comment model

--- articles/models.py
+++ articles/models.py
@@ -20,3 +20,18 @@ class Articles(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse("article_list")
