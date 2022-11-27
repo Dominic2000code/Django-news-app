065a5c0c43f0bba3397686e5cdcf4a1e9b0ec06f
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-27 06:38:42 +0000

    added view to list articles

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Articles
 # Create your views here.
+
+
+class ArticleListView(ListView):
+    model = Articles
+    template_name = 'article_list.html'
