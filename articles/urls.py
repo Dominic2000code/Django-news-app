00c0a94af9b5e51a7c9addb95931436c406ba486
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-27 08:54:14 +0000

    added urls for article mutation

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleUpdateView,  # new
+    ArticleDetailView,  # new
+    ArticleDeleteView,  # new
+)
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list')
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('', ArticleListView.as_view(), name='article_list'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Articles
 # Create your views here.
 
@@ -6,3 +8,20 @@ from .models import Articles
 class ArticleListView(ListView):
     model = Articles
     template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):  # new
+    model = Articles
+    template_name = 'article_detail.html'
+
+
+class ArticleUpdateView(UpdateView):  # new
+    model = Articles
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+
+class ArticleDeleteView(DeleteView):  # new
+    model = Articles
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
