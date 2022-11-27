22f2fc1d8a52eb3dd6fc4d9fd235402370284457
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-27 13:24:12 +0000

    added functionality to create new article

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Articles
 # Create your views here.
@@ -10,18 +10,24 @@ class ArticleListView(ListView):
     template_name = 'article_list.html'
 
 
-class ArticleDetailView(DetailView):  # new
+class ArticleDetailView(DetailView):
     model = Articles
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):  # new
+class ArticleUpdateView(UpdateView):
     model = Articles
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
 
-class ArticleDeleteView(DeleteView):  # new
+class ArticleDeleteView(DeleteView):
     model = Articles
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+    model = Articles
+    template_name = 'article_new.html'
+    fields = ('title', 'body', 'author',)
